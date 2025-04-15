fc52c35aedec42443df8e98742dd4e8160bbfde8
sibadima <148964526+sibadima@users.noreply.github.com>
2025-04-16 00:06:36 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,34 +1,35 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        PasswordChecker checker = new PasswordChecker();
+    public class Main {
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            PasswordChecker checker = new PasswordChecker();
 
-        try {
-            System.out.println("Введите мин. длину пароля: ");
-            int minLength = Integer.parseInt(sc.nextLine());
-            checker.setMinLength(minLength);
+            try {
+                System.out.println("Введите мин. длину пароля: ");
+                int minLength = Integer.parseInt(sc.nextLine());
+                checker.setMinLength(minLength);
 
-            System.out.println("Введите макс. допустимое количество повторений символа подряд: ");
-            int maxRepeats = Integer.parseInt(sc.nextLine());
-            checker.setMaxRepeats(maxRepeats);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
+                System.out.println("Введите макс. допустимое количество повторений символа подряд: ");
+                int maxRepeats = Integer.parseInt(sc.nextLine());
+                checker.setMaxRepeats(maxRepeats);
 
-        try {
-            while (true) {
-                System.out.print("Введите пароль или end: ");
-                String password = sc.nextLine();
-                if ("end".equals(password)) {
-                    break;
-                }
-
-                System.out.println(checker.verify(password) ? "Подходит" : "Не подходит!");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+
+            try {
+                while (true) {
+                    System.out.print("Введите пароль или end: ");
+                    String password = sc.nextLine();
+                    if ("end".equals(password)) {
+                        break;
+                    }
+
+                    System.out.println(checker.verify(password) ? "Подходит" : "Не подходит!");
+                }
+            } catch (IllegalStateException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
-}
